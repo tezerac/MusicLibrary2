9f6ce6c2ecfc79b3b4c271c0969574905a675e4f
tezerac <41699924+tezerac@users.noreply.github.com>
2018-09-01 18:31:59 -0700

    New comit

---
+++ MusicLibrary_app/Music.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary_app
+{
+    class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public static Music GetMusic()
+        {
+            var music = new Music
+            {
+                Title = "selam",
+                Artist = "Mohamud",
+                Album = "Ethiopia"
+            };
+            return music;
+        }
+    }
+}
